b03093e25f3c7b1484a40264c54be5f31f6ec39a
liuredflash <522903052@qq.com>
2019-02-17 10:46:56 +0800

    完成es的CRUD

--- es-prj-java/src/main/java/com/company/es/es_prj_java/EmployeeCRUDApp.java
+++ es-prj-java/src/main/java/com/company/es/es_prj_java/EmployeeCRUDApp.java
@@ -3,9 +3,16 @@ package com.company.es.es_prj_java;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateAction;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 public class EmployeeCRUDApp {
 
@@ -24,7 +31,38 @@ public class EmployeeCRUDApp {
 	}
 	
 	private void createEmployee(TransportClient client) throws Exception{
-		client.prepareIndex()
+		IndexResponse response = client.prepareIndex("company", "employee", "1")
+		    .setSource(XContentFactory.jsonBuilder()
+		    		.startObject()
+		    		.field("name", "jack")
+		    		.field("age", 27)
+		    		.field("position", "technique")
+		    		.field("country", "china")
+		    		.field("join_date", "2019-01-01")
+		    		.field("salary", 10000)
+		    		.endObject())
+		    .get();
+		System.out.println(response.getResult());
 	}
-
+	
+	private void getEmployee(TransportClient client) throws Exception{
+		GetResponse response = client.prepareGet("company", "employee", "1").get();
+		System.out.println(response.getSourceAsString());
+	}
+	
+	private void updateEmployee(TransportClient client) throws Exception{
+		UpdateResponse response = client.prepareUpdate("company", "employee", "1")
+		.setDoc(XContentFactory.jsonBuilder()
+				.startObject()
+				.field("position", "technique")
+				.endObject())
+		.get();
+		
+		System.out.println(response.getResult());
+	}
+	
+	private void deleteEmployee(TransportClient client) throws Exception{
+		DeleteResponse response = client.prepareDelete("company", "employee", "1").get();
+		System.out.println(response.getResult());
+	}
 }
